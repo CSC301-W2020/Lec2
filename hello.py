c7201a6c9d4829abda51302ff6114b3b9211349c
Even Zhou <even.y.zhou@outlook.com>
2020-01-15 13:48:49 -0500

    add a new line to hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
-#!/bin/python
+#!/usr/bin/env python3
 
 if __name__ == "__main__":
     print("Hello class! I am your worst nightmare!!!!")
     print("Hello class!!!!! I am your worst nightmare")
+    print("I love CSC301!")
